7257b65726e587bf86faaafa36d36bcaf55af061
Desmond <desmond.agye@gmail.com>
2019-03-07 15:55:56 +0000

    pass data between fragments

--- app/src/main/java/com/challenge/static_fragments/MainActivity.java
+++ app/src/main/java/com/challenge/static_fragments/MainActivity.java
@@ -1,13 +1,21 @@
 package com.challenge.static_fragments;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TopFragment.OnButtonClickedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onTopFragmentButtonClicked(String message) {
+        BottomFragment bottomFragment = (BottomFragment) getSupportFragmentManager().findFragmentById(R.id.static__bottom_fragment);
+        if (bottomFragment != null) {
+            bottomFragment.onMessageReceived(message);
+        }
     }
 }
--- app/src/main/java/com/challenge/static_fragments/TopFragment.java
+++ app/src/main/java/com/challenge/static_fragments/TopFragment.java
@@ -1,17 +1,29 @@
 package com.challenge.static_fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TopFragment extends Fragment {
+
+    private Button sendMessageButton;
+
+    interface OnButtonClickedListener {
+        void onTopFragmentButtonClicked(String message);
+    }
+
+    OnButtonClickedListener listener;
 
 
     public TopFragment() {
@@ -24,8 +36,38 @@ public class TopFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_hellow_world, container, false);
-        view.findViewById(R.id.hello_textview);
+        sendMessageButton = view.findViewById(R.id.send_message_button);
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        sendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onTopFragmentButtonClicked("I have been clicked!");
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Make sure the container activity implements the callback interface
+        MainActivity activity = (MainActivity) context;
+        try {
+            if (context instanceof OnButtonClickedListener) {
+                listener = (OnButtonClickedListener) context;
+            }
+        } catch (ClassCastException exception) {
+            throw new ClassCastException(activity.toString() + " must implement OnButtonClickedListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        listener = null;
+        super.onDetach();
+    }
 }
